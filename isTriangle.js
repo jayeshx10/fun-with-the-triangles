acf62ce8c18ceebafa13f22a7879e6045a47c67e
Jayesh Patil <jayeshpatil292@gmail.com>
2022-12-01 16:06:16 +0530

    semantics updated

--- isTriangle.js
+++ isTriangle.js
@@ -1,4 +1,4 @@
-const angles = document.querySelectorAll(".angle-inputs");
+const angles = document.querySelectorAll(".input-fields");
 const checkBtn = document.querySelector("#tri-btn");
 var output = document.querySelector(".output-div");
 
@@ -6,33 +6,41 @@ function calculateSum(ang1, ang2, ang3){
     return ang1 + ang2 + ang3;
 }
 
+function clearInputs(){
+    for(let angle of angles){
+        angle.value = "";
+}
+}
+
+function displayMsg(color, msg){
+    output.style.display = "block";
+    output.style.color = color;
+    output.innerHTML = msg;
+}
+
 function isTriangle(){
     if(angles[0].value == "" || angles[1].value == "" ||angles[2].value == ""){
-        output.style.display = "block";
-        output.style.color = "red";
-        output.innerHTML = "Please enter all the fields";
+        displayMsg("red", "Please enter all the fields");
     }
     else if(Number(angles[0].value) === 0 || Number(angles[1].value) === 0 || Number(angles[2].value) === 0){
-        output.style.display = "block";
-        output.style.color = "red";
-        output.innerHTML = "C'mon macha, angles cannot be ZERO na !";
+        displayMsg("red", "C'mon macha, angles cannot be ZERO na !");
+        clearInputs();
     }
     else{
         if(Number(angles[0].value) < 0 || Number(angles[1].value) < 0 || Number(angles[2].value) < 0){
-            output.style.display = "block";
-            output.style.color = "red";
-            output.innerHTML = "C'mon macha, angles cannot be NEGATIVE na !";
+            displayMsg("red", "C'mon macha, angles cannot be NEGATIVE na !");
+            clearInputs();
         }
         else{
             var sum = calculateSum(Number(angles[0].value),  Number(angles[1].value), Number(angles[2].value));
 
             if(sum === 180){
-                output.style.display = "block";
-                output.innerHTML = "Yes, the angles are forming a triangle !!!";
+                displayMsg("black", "Yes, the angles are forming a triangle !!!");
+                clearInputs();
             }
             else{
-                output.style.display = "block";
-                output.innerHTML = "Nope, the angles are NOT forming a triangle !!!";
+                displayMsg("black", "Nope, the angles are NOT forming a triangle !!!");
+                clearInputs();
             }
         }
     }
